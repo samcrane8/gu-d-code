709784503d2c9e4f9c4f30ef7f93582ec3094175
Bruce Crane <samcrane8@gmail.com>
2017-12-12 20:24:14 -0500

    finished connecting back-parts
    - got join button working
    - got instagram working
    - got Facebook working

--- front-end/src/components/Splash.vue
+++ front-end/src/components/Splash.vue
@@ -25,6 +25,7 @@
                   <v-text-field
                     label="Email"
                     style="padding-left:15%;padding-right:10%;"
+                    v-model="email"
                   />
                   <v-flex class="text-xs-center">
                     <v-btn outline style="border-radius:0px;background-color:#f4b061;"type="submit" @click.prevent = "join">JOIN</v-btn>
@@ -38,12 +39,12 @@
                       <path fill="#000000" d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" />
                     </svg>
                   </v-btn> -->
-                  <v-btn flat style="border-radius:2px;"type="submit" @click.prevent = "instagram">
+                  <v-btn flat style="border-radius:2px;"type="submit" href="http://www.instagram.com">
                     <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                       <path fill="#000000" d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
                     </svg>
                   </v-btn>
-                  <v-btn flat style="border-radius:2px;"type="submit" @click.prevent = "facebook">
+                  <v-btn flat style="border-radius:2px;"type="submit" href="http://www.facebook.com">
                     <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                       <path fill="#000000" d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z" />
                     </svg>
@@ -72,26 +73,18 @@ export default {
     return {
       auth: {"username": '', "password": ''},
       response: {},
+      email: ""
     }
   },
   methods: {
     join: function() {
-      var url = "http://54.173.144.94:5000/login"
+      var url = "http://gudlinens.com:5000/add_to_newsletter"
       const vm = this;
-      var credentials = btoa(this.auth.username + ':' + this.auth.password);
-      var BasicAuth = 'Basic ' + credentials;
-      axios.get(url, {
-        headers: {'Authorization': BasicAuth}
-      })
+      var body = { 'email': this.email}
+      axios.post(url, body)
         .then((response) => {
           if (response.data.code == 200) {
-            var userinfo = {
-              auth: vm.auth,
-              isadmin: response.data.isAdmin
-            }
-            vm.$emit('login',userinfo)
-          } else {
-            alert('Bad Login!')
+            this.email = ""
           }
         })
         .catch(error => {
